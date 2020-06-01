2bc35e7238387e9533230c449bf0993ee53ff86e
Chotle <60724357+lemac2002@users.noreply.github.com>
2020-05-31 22:19:29 -0400

    Update main.js

--- jirabot/scripts/main.js
+++ jirabot/scripts/main.js
@@ -39,34 +39,4 @@ module.exports = function(robot) {
 		.catch(err => console.error(err));
 		}
 	});
-	/*robot.respond(/help/i, function(msg){
-		var help = "Here's the list of available commands:\n\n";
-		help+="help -- lists available commands\n\n";
-		help+="list -- returns a list of all the issues you've reported\n\n";
-		help+="details <issuekey> -- returns details for the specified issue\n\n";
-		help+="status <status> -- returns a list of issues with the specified status";
-		msg.reply(help);
-		
-	});
-	robot.respond(/list/i, function(msg){
-		// This code sample uses the 'node-fetch' library:
-		// https://www.npmjs.com/package/node-fetch
-		var user = "mpaquette"
-		fetch('https://techinsights.atlassian.net/rest/api/3/search?jql=project%3DSWSD%20AND%20reporter%3D'+user, {
-			method: 'GET',
-			headers: {
-				'Authorization': `Basic ${Buffer.from('iprou1@ocdsb.ca:0904zOBp5x4QVNBIe1h638F4').toString('base64')}`,
-				'Accept': 'application/json'
-			}
-
-		})
-		.then(response => {
-			console.log(
-				`Response: ${response.status} ${response.statusText}`
-			);
-			return response.text();
-		})
-		.then(text => msg.reply(List(text.split("{\"expand\":"))))
-		.catch(err => console.error(err));
-	});*/
-}
+	
